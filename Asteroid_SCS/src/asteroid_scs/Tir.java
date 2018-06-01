576329c1804846b1bd7f07ab07e253b967bed1db
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2018-06-01 13:57:28 +0200

    x

---
+++ Asteroid_SCS/src/asteroid_scs/Tir.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asteroid_scs;
+
+import iut.Jeu;
+import iut.Objet;
+import iut.ObjetTouchable;
+
+/**
+ *
+ * @author eg816621
+ */
+public class Tir extends ObjetTouchable {
+
+    public Tir(Jeu g, String nom, int x, int y) {
+        super(g, nom, x, y);
+    }
+
+    @Override
+    public void effetCollision(Objet objet) {
+        
+    }
+
+    @Override
+    public String getTypeObjet() {
+       
+    }
+
+    @Override
+    public void evoluer(long l) {
+        
+    }
+    
+}
