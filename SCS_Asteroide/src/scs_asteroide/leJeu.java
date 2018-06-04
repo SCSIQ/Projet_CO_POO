6a3a7a522cb51077113dc7dbf279ac832435a9e8
Aurian Durand <34919475+AurianDurand@users.noreply.github.com>
2018-06-04 09:00:41 +0200

    correction bugs

--- SCS_Asteroide/src/scs_asteroide/leJeu.java
+++ SCS_Asteroide/src/scs_asteroide/leJeu.java
@@ -8,6 +8,8 @@ package scs_asteroide;
 import iut.Jeu;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 /**
  *
@@ -16,36 +18,29 @@ import java.awt.Graphics;
 public class leJeu extends Jeu{
 
     //ATTRIBUTS 
-    private Vaisseau vaisseau;
-    private Jeu jeu ;
+    private Vaisseau vaisseau = null;
     private int score = 0;
     private int vie = 3;
     
     public leJeu(int largeur, int hauteur, String title) {
-        super(largeur, hauteur, title);
-        vaisseau = new Vaisseau(this, "joueur", 0, 0);
-        dessinerArrierePlan(this.getGraphics());
-        creeObjets();
-        
+        super(largeur, hauteur, title); 
     }
 
     @Override
     protected void creeObjets() {
-        vaisseau.dessiner(this.getGraphics(),10,10);
+        vaisseau = new Vaisseau(this, "fusee", 0, 0);
+        this.ajouteEcouteurClavier(vaisseau);
+        this.ajouter(vaisseau);
         
-        PetitAsteroide comete = new PetitAsteroide(jeu ,"comete",20,20 ); 
-
-        
-        for(int i=1;i<6;i++){
+        //PetitAsteroide comete = new PetitAsteroide(jeu ,"comete",20,20 ); 
+        /*for(int i=1;i<6;i++){
             comete.dessiner(this.getGraphics());
-        }
+        }*/
     }
 
     @Override
     protected void dessinerArrierePlan(Graphics grphcs) {
         grphcs.setColor(Color.black);
-        grphcs.fillRect(0, 0, 500, 500);
-        grphcs.fillRect(0, 0, 500, 500);
         grphcs.fillRect(0, 0, 500, 500);
         grphcs.setColor(Color.white);
         grphcs.drawString("Vies : ", 300, 15);
@@ -68,12 +63,20 @@ public class leJeu extends Jeu{
 
     @Override
     protected boolean aGagne() {
-      return true ; 
+      return false ; 
     }
 
     @Override
     protected boolean aPerdu() {
        return false ; 
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setVie(int vie) {
+        this.vie = vie;
     }
     
 }
