e8168d9ce6add1bd72ac3f2cef1b8ed0540ab382
BrightHammer21 <lucieboucquemont@hotmail.fr>
2018-06-04 10:23:13 +0200

    z

--- SCS_Asteroide/src/scs_asteroide/leJeu.java
+++ SCS_Asteroide/src/scs_asteroide/leJeu.java
@@ -51,7 +51,6 @@ public class leJeu extends Jeu{
         this.ajouter(ma);        
         this.ajouter(ga);
         
-        
         Vie v = new Vie(this, "trois_coeur", 400, 400);
         //this.ajouter(v);
        
