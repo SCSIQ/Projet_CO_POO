79f79c9e1541bf1d6fdf206c62ab6ae73ffd9ecd
AlexianeIUT <35493278+AlexianeIUT@users.noreply.github.com>
2018-06-04 10:15:43 +0200

    a

--- SCS_Asteroide/src/scs_asteroide/Vaisseau.java
+++ SCS_Asteroide/src/scs_asteroide/Vaisseau.java
@@ -58,17 +58,12 @@ public class Vaisseau extends ObjetTouchable implements KeyListener {
                     this.deplacerXY(0, +10);
                 break; 
             case KeyEvent.VK_SPACE:
-<<<<<<< HEAD
-               // if (this.)
-                        break ; 
-=======
-
                 if (this.posDroite()<this.leJeu().largeur()){
                 Tir t = new Tir(this.leJeu(), "tir", 80, 20) ; 
                 leJeu().ajouter(t); 
             }
                         break ;           
->>>>>>> 7ada20e7cd922703e221eb54c466f25ad200a6b1
+
         }
          
             //KeyEvent.VK_Z;
